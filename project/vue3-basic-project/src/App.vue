55bfb835e16e2ebbd69b43c6ed47e024862794e1
stupid-child111 <3468619937@qq.com>
2025-03-01 15:47:46 +0800

    *编辑功能完成*

--- project/vue3-basic-project/src/App.vue
+++ project/vue3-basic-project/src/App.vue
@@ -17,13 +17,26 @@ onMounted(() => {
 // TODO: 删除功能
 //思路：点击删除按钮-->获取当前行的id-->通过id调用删除接口-->更新最新的列表
 const onDel = async(id) =>{
-  console.log(id);
+  // console.log(id);
   await axios.delete(`/del/${id}`)
   getList()
 }
 
 // TODO: 编辑功能
+//思路：点击编辑按钮-->弹出编辑框-->编辑框中显示当前行的数据-->修改数据-->调用编辑接口-->更新最新的列表
+//思路：打开弹框-->回填数据-->更新数据
+//1.打开弹框(获取子组件实例————>调用方法或者修改属性)
+//(1)生成ref对象
+//(2)绑定到子组件
+const editRef = ref(0)
 
+
+const onEdit = (row) =>{
+  //事件发生的时候拿到子组件实例对象，调用open方法
+  editRef.value.open(row)
+}
+
+//2.回调数据(显示出详情)  两种方法(调用详情数据 / 当前行的静态数据)
 </script>
 
 <template>
@@ -35,13 +48,13 @@ const onDel = async(id) =>{
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
         <template #default="{ row }">
-          <el-button type="primary" link>编辑</el-button>
+          <el-button type="primary" @click="onEdit(row)" link>编辑</el-button>
           <el-button type="danger" @click="onDel(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
   </div>
-  <Edit />
+  <Edit ref="editRef" @onUpdate="getList"/>
 </template>
 
 <style scoped>
--- project/vue3-basic-project/src/components/Edit.vue
+++ project/vue3-basic-project/src/components/Edit.vue
@@ -1,26 +1,64 @@
 <script setup>
 // TODO: 编辑
 import { ref } from 'vue'
+import axios from 'axios'
 // 弹框开关
 const dialogVisible = ref(false)
 
+//数据绑定到表单上，准备当前组件下的响应式数据
+const form = ref({
+  name: '',
+  place: ''
+})
+//拿到实例，一般不直接修改属性而是通过调用方法(灵活通)
+const open = (row) =>{
+  // console.log(row)
+  form.value.name = row.name
+  form.value.place = row.place
+  form.value.id = row.id
 
+  //写完后，模板区域进行双向绑定
+  //第二步回填数据完成
+  dialogVisible.value = true
+}
+//组件和方法默认是封闭的，通过defineExpose暴露出去
+defineExpose({
+  open
+})
+
+//3. 更新数据
+const emit = defineEmits(['onUpdate'])
+
+const onUpdate = async() =>{
+  //(1)收集表单数据 调用接口
+  await axios.patch(`/edit/${form.value.id}`, {
+  name: form.value.name,
+  place: form.value.place,
+  id: form.value.id
+})
+  //(2)关闭弹框
+  dialogVisible.value = false
+  //(3)通知父组件更新列表(子传父) 
+  //调用编译器宏defineEmits
+  emit('onUpdate')
+  
+}
 </script>
 
 <template>
   <el-dialog v-model="dialogVisible" title="编辑" width="400px">
     <el-form label-width="50px">
       <el-form-item label="姓名">
-        <el-input placeholder="请输入姓名" />
+        <el-input placeholder="请输入姓名" v-model="form.name"/>
       </el-form-item>
       <el-form-item label="籍贯">
-        <el-input placeholder="请输入籍贯" />
+        <el-input placeholder="请输入籍贯" v-model="form.place"/>
       </el-form-item>
     </el-form>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
-        <el-button type="primary" @click="dialogVisible = false">确认</el-button>
+        <el-button type="primary" @click="onUpdate">确认</el-button>
       </span>
     </template>
   </el-dialog>
