e3688227207f791376a86e5c686cbda9f748b564
stupid-child111 <3468619937@qq.com>
2025-03-01 19:17:03 +0800

    storeToRefs以及Pinia调试

--- project/vue-pinia/src/App.vue
+++ project/vue-pinia/src/App.vue
@@ -1,11 +1,23 @@
 <script setup>
 import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia';
 //1.导入use命名的方法
 import { useCounterStore } from './stores/counter';
 //2.执行方法得到store实例对象
 const counterStore = useCounterStore();
-console.log(counterStore)
+// console.log(counterStore)
+//直接解构赋值
+// const { count, doubleCount } = counterStore
+// console.log(count, doubleCount);
 
+//方法包裹
+const { count, doubleCount } = storeToRefs(counterStore)
+console.log(count, doubleCount);
+
+//方法直接从原来的counterStore中解构
+const {increment} = counterStore
+
+//触发action
 onMounted(() =>{
   counterStore.getList()
 })
@@ -13,8 +25,8 @@ onMounted(() =>{
 
 <template>
   <div>
-    <button @click="counterStore.increment">{{counterStore.count}}</button>
-    {{ counterStore.doubleCount }}
+    <button @click="increment">{{count}}</button>
+    {{ doubleCount }}
     <ul>
       <li v-for="item in counterStore.list" :key="item.id">{{ item.name }}</li>
     </ul>
