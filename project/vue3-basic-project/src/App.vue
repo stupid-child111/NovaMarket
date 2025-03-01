d9525c6231552f6ae567835c5646a8d54a7f3f61
stupid-child111 <3468619937@qq.com>
2025-03-01 13:59:28 +0800

    删除功能完成

--- project/vue3-basic-project/src/App.vue
+++ project/vue3-basic-project/src/App.vue
@@ -15,7 +15,12 @@ onMounted(() => {
   getList();
 })
 // TODO: 删除功能
-
+//思路：点击删除按钮-->获取当前行的id-->通过id调用删除接口-->更新最新的列表
+const onDel = async(id) =>{
+  console.log(id);
+  await axios.delete(`/del/${id}`)
+  getList()
+}
 
 // TODO: 编辑功能
 
@@ -29,9 +34,9 @@ onMounted(() => {
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
+        <template #default="{ row }">
           <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+          <el-button type="danger" @click="onDel(row.id)" link>删除</el-button>
         </template>
       </el-table-column>
     </el-table>
