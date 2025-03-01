0fdd5ea3cdb60142c10073e1b942ba8c6e6d5bd4
stupid-child111 <3468619937@qq.com>
2025-03-01 17:11:24 +0800

    getters和异步action

--- project/vue-pinia/src/App.vue
+++ project/vue-pinia/src/App.vue
@@ -1,14 +1,23 @@
 <script setup>
+import { onMounted } from 'vue'
 //1.导入use命名的方法
 import { useCounterStore } from './stores/counter';
 //2.执行方法得到store实例对象
 const counterStore = useCounterStore();
 console.log(counterStore)
+
+onMounted(() =>{
+  counterStore.getList()
+})
 </script>
 
 <template>
   <div>
     <button @click="counterStore.increment">{{counterStore.count}}</button>
+    {{ counterStore.doubleCount }}
+    <ul>
+      <li v-for="item in counterStore.list" :key="item.id">{{ item.name }}</li>
+    </ul>
   </div>
 </template>
 
--- project/vue-pinia/src/stores/counter.js
+++ project/vue-pinia/src/stores/counter.js
@@ -1,6 +1,9 @@
 //导入一个方法definestore
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed,ref } from 'vue'
+import axios from 'axios'
+
+const API_URL = 'http://geek.itheima.net/v1_0/channels'
 export const useCounterStore = defineStore('counter', () => {
     //定义数据(state)
     const count = ref(0)
@@ -12,9 +15,23 @@ export const useCounterStore = defineStore('counter', () => {
     const increment = () => {
         count.value++
     }
+
+    //getter定义(computed)
+    const doubleCount = computed(() =>count.value * 2)
+
+    //定义异步action
+    const list = ref([])
+    const getList = async() => {
+        const res = await axios.get(API_URL)
+        list.value = res.data.data.channels
+    }
+
     //以对象的方式teturn供组件使用
     return {
         count,
+        doubleCount,
+        list,
+        getList,
         increment
     }
 })  
