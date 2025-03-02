1b7b01f2181b5f680aece89420d71f34dcedef68
stupid-child111 <3468619937@qq.com>
2025-03-02 14:18:30 +0800

    axios基础封装

--- project/vue-NovaMarket/src/main.js
+++ project/vue-NovaMarket/src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
+//测试接口函数
+import {getCategory} from '@/apis/testAPI'
+getCategory().then(res=>{
+    console.log(res)
+})
 createApp(App).mount('#app')
