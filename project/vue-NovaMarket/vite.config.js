9f239f79bbaf4c51daffc6511ff9ec3966880164
stupid-child111 <3468619937@qq.com>
2025-03-02 13:50:14 +0800

    Element主题色配置

--- project/vue-NovaMarket/vite.config.js
+++ project/vue-NovaMarket/vite.config.js
@@ -1,3 +1,4 @@
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -16,7 +17,26 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [ElementPlusResolver({ importStyle: 'sass' })],
     }),
+    // // 按需定制主题配置
+    // ElementPlus({
+    //   useSource: true,
+    // }),
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入定制化样式文件进行样式覆盖
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+        `,
+      }
+    }
+  }
 })
