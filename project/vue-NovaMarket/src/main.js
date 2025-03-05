7803e71dc4eed678ec50983e0bf2e186dfb5f63b
stupid-child111 <3468619937@qq.com>
2025-03-05 18:41:52 +0800

    图片懒加载实现

--- project/vue-NovaMarket/src/main.js
+++ project/vue-NovaMarket/src/main.js
@@ -3,14 +3,40 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
+//引入初始化样式文件
+import '@/styles/common.scss'
+
+import { useElementBounding, useIntersectionObserver } from '@vueuse/core'
 
 const pinia = createPinia()
 
-//引入初始化样式文件
-import '@/styles/common.scss'
+
 // //测试接口函数
 // import {getCategory} from '@/apis/testAPI'
 // getCategory().then(res=>{
 //     // console.log(res)
 // })
-createApp(App).use(router).use(pinia).mount('#app')
+
+const app = createApp(App)
+
+app.use(router)
+app.use(pinia)
+app.mount('#app')
+//定义全局指令
+app.directive('img-lazy', {
+    mounted(el,binding){
+        //el:指令绑定的哪个元素 img
+        //binding:  常用binding.value 指令等于号后面绑定的表达式的值  图片url
+        console.log(el,binding.value)
+        useIntersectionObserver(
+            el,
+            ([{isIntersecting}]) => {
+                console.log(isIntersecting)
+                if(isIntersecting){
+                    //进入视口区域
+                    el.src = binding.value
+                }
+            }
+        )
+    }
+});
