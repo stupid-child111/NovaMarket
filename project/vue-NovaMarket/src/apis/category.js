0f11136f7cca7e1359d743a68acf788511040307
stupid-child111 <3468619937@qq.com>
2025-03-11 16:50:16 +0800

    二级分类-基础列表渲染

--- project/vue-NovaMarket/src/apis/category.js
+++ project/vue-NovaMarket/src/apis/category.js
@@ -1,14 +1,23 @@
 import httpInstance from '@/utils/http'
-import req from '@/utils/http'
 
 export function getCategoryAPI(id){
-    return req({
+    return httpInstance({
         url: '/category',
         params: {
             id
         }
     })
 }
+// import req from '@/utils/http'
+
+// export function getCategoryAPI(id){
+//     return req({
+//         url: '/category',
+//         params: {
+//             id
+//         }
+//     })
+// }
 
 /**
  * @description: 获取二级分类列表数据
@@ -22,5 +31,24 @@ export const getCategoryFilterAPI = (id) => {
       params:{
         id
       }
+    })
+  }
+
+
+/**
+ * @description: 获取导航数据
+ * @data { 
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   } 
+ * @return {*}
+ */
+   export const getSubCategoryAPI = (data) => {
+    return httpInstance({
+      url:'/category/goods/temporary',
+      method:'POST',
+      data
     })
   }
