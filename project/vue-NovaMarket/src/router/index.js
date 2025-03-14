674b91f177103dc6758e6fa6d20c6c85c1664d67
stupid-child111 <3468619937@qq.com>
2025-03-14 22:12:00 +0800

    详情-整体业务认识

--- project/vue-NovaMarket/src/router/index.js
+++ project/vue-NovaMarket/src/router/index.js
@@ -8,6 +8,7 @@ import Home from "@/views/home/index.vue"
 import Category from "@/views/category/index.vue"
 
 import SubCategory from '@/views/subCategory/index.vue'
+import Detail from '@/views/detail/index.vue'
 
 const router = createRouter({
     history:createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,10 @@ const router = createRouter({
                 {
                     path:"category/sub/:id",
                     component: SubCategory
+                },
+                {
+                    path: 'detail/:id',
+                    component: Detail
                 },
             ]
 
